36b8a4707d10fffd0ea0350b5c604a8d58c4488c
bbaumgar <bwb8ta@virginia.edu>
2013-09-30 14:55:35 -0400

    Add homeHandler and print message to show server is running

--- server.go
+++ server.go
@@ -2,17 +2,23 @@
 package main
 
 import (
-    "flag"
-    "net/http"
+	"flag"
+	"fmt"
+	"net/http"
 )
 
 var port = flag.String("port", "8080", "Define what TCP port to bind to")
 var root = flag.String("root", ".", "Define the root filesystem path")
 
 func main() {
-    http.HandleFunc("/", homeHandler)
-    http.HandleFunc("/parse", func(w http.ResponseWriter, r *http.Request) {
-        http.ServeFile(w, r, r.URL.Path[1:])
-    })
-    panic(http.ListenAndServe(":7331", nil))
+	http.HandleFunc("/parse", homeHandler)
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, r.URL.Path[1:])
+	})
+	panic(http.ListenAndServe(":7331", nil))
+    fmt.Println("Now running summarizer on port 7331.")
+}
+
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello, world.")
 }
